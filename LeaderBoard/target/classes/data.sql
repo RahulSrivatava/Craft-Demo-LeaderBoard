34c23a74d6ca0e9dccf89e7c3d1983c74a3bb392
Rahul Srivastava <rahul.srivastava12134@gmail.com>
2024-08-20 20:21:18 +0530

    Added unit test

---
+++ LeaderBoard/target/classes/data.sql
@@ -0,0 +1,9 @@
+TRUNCATE TABLE player_goal;
+INSERT INTO player_goal (player_Id, goal)
+VALUES ("Rohit", 3),
+        ("Nishchhal", 2),
+        ("Shivam", 1),
+        ("Brijesh", 6);
+Select  * from player_goal;
+show tables;
+show Databases ;
